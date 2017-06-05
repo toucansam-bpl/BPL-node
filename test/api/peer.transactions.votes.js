4dad9dcfcd5e75806e75cb8ead28f9ab1a7ad5d2
Raj <rajveer.singh@wsu.edu>
2017-06-05 11:05:07 -0700

    Updating as per BPL-Desktop

--- test/api/peer.transactions.votes.js
+++ test/api/peer.transactions.votes.js
@@ -60,7 +60,7 @@ function postVote (transaction, done) {
 	});
 }
 
-function sendArk (params, done) {
+function sendBpl (params, done) {
 	node.put('/api/transactions', params, function (err, res) {
 		node.expect(res.body).to.have.property('success').to.be.ok;
 		node.onNewBlock(function (err) {
@@ -84,7 +84,7 @@ function registerDelegate (account, done) {
 describe('POST /peer/transactions', function () {
 
 	before(function (done) {
-		sendArk({
+		sendBpl({
 			secret: node.gAccount.password,
 			amount: 100000000000,
 			recipientId: account.address
