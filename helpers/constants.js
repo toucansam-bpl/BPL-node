fba2f5509ea9731de1c18b9a3cb5b126609f1f22
Marilyn Pereira <marilyn.pereira@harbingergroup.com>
2017-06-25 20:02:56 +0530

    Added BPL-explorer url reference. Maximum number of votes set to 1.

--- helpers/constants.js
+++ helpers/constants.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	activeDelegates: 135, //changed from 51 to 135
-	maximumVotes: 135,
+	maximumVotes: 1,
 	addressLength: 208,
 	blockHeaderLength: 248,
 	confirmationLength: 77,
@@ -31,17 +31,17 @@ module.exports = {
 	requestLength: 104,
 	rewards: {
 		milestones: [
-			200000000, // Initial Reward
-			200000000, // Milestone 1
-			200000000, // Milestone 2
-			200000000, // Milestone 3
-			200000000, // Milestone 4
-			200000000  // Milestone 5
+			5, // Initial Reward
+			4, // Milestone 1
+			3, // Milestone 2
+			2, // Milestone 3
+			1, // Milestone 4
+			0  // Milestone 5
 		],
 		offset: 40320,   // changed from 75600 to 40320 // Start rewards at block, ie 7 days after net start
 		distance: 3000000, // Distance between each milestone
 	},
 	signatureLength: 196,
-	totalAmount: 2500000000000000, // changed from 12500000000000000 to 2500000000000000
+	totalAmount: 12500000000000000, // changed from 12500000000000000 to 2500000000000000
 	unconfirmedTransactionTimeOut: 10800 // 1080 blocks
 };
