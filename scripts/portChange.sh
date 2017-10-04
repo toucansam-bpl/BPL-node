be6412b9c4a28b345e6e17cc164677fe002baf71
marilynpereira03 <marilyn.pereira@harbingergroup.com>
2017-10-04 19:52:15 +0530

    Updated port change script.

--- scripts/portChange.sh
+++ scripts/portChange.sh
@@ -8,6 +8,6 @@ git checkout config.testnet.json
 # Copy contents from newconfig to config file
 cp newconfig.testnet.json config.testnet.json
 # Run the node using forever command
-forever start app.js -c newconfig.testnet.json -g genesisBlock.testnet.json > a.txt
+forever start app.js -c config.testnet.json -g genesisBlock.testnet.json
 # Kill existing node process running on port 4000
 kill `lsof -t -i:4000`
