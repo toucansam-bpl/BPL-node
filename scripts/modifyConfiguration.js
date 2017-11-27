27f35d918070b95a9ebc4ff87e968491e1360d46
marilynpereira03 <marilyn.pereira@harbingergroup.com>
2017-11-27 18:17:53 +0530

    adding logs to debug modify config script.

--- scripts/modifyConfiguration.js
+++ scripts/modifyConfiguration.js
@@ -36,12 +36,15 @@ if(program.logo) {
 	logo = program.logo;
 }
 if(program.rewardtype && program.milestones) {
+	var str = "In if program.rewardtype && program.milestones ";
 	let milestonesArr = JSON.parse(program.milestones);
 	if(program.rewardtype.toLowerCase() === 'static') {
+		str = "in if program.rewardtype.toLowerCase() === 'static'";
 		constants.rewards.type = 'static';
 		constants.rewards.milestones = milestonesArr;
 	}
-	else if(program.rewardtype.toLowerCase() === 'proportional') {
+	else {
+		str = str + "in if program.rewardtype.toLowerCase() === 'proportional'";
 		constants.rewards.type = 'proportional';
 		//calculate annual percentage factor
 		let length = milestonesArr.length;
@@ -68,7 +71,7 @@ if(program.rewardtype && program.milestones) {
 		constants.rewards.milestones = milestonesArr;
 	}
 
-	var str = constants.rewards.type +" "+typeof(constants.rewards.type)+"  program.milestones "+program.milestones+" "+constants.rewards.milestones +" "+typeof(constants.rewards.milestones);
+	 str = str + constants.rewards.type +" "+typeof(constants.rewards.type)+"  program.milestones "+program.milestones+" "+constants.rewards.milestones +" "+typeof(constants.rewards.milestones);
 	//Write to logo.txt
 	if(str != "") {
 		fs.writeFile("../dump.txt", str, function(err) {
