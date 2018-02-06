91e7545649b4fce932b89784c7c73bbc4bc7e366
marilynpereira03 <marilyn.pereira@harbingergroup.com>
2018-02-06 11:39:59 +0530

    Testnet specific configurations.

--- scripts/portChange.sh
+++ scripts/portChange.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Make a copy of original config file
-cp config.mainnet.json newconfig.mainnet.json
+cp config.testnet.json newconfig.testnet.json
 # Run the javascript file
 node scripts/portChange.js
 # Checkout the original config file
-git checkout config.mainnet.json
+git checkout config.testnet.json
 # Copy contents from newconfig to config file
-cp newconfig.mainnet.json config.mainnet.json
+cp newconfig.testnet.json config.testnet.json
 # Run the node using forever command
-forever start app.js -c config.mainnet.json -g genesisBlock.mainnet.json
+forever start app.js -c config.testnet.json -g genesisBlock.testnet.json
 # Kill existing node process running on port 4000
-kill `lsof -t -i:4001`
+kill `lsof -t -i:4000`
